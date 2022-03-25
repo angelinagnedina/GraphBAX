ca8f56d28a0fe5b75d829ef49e62cdd781d835ef
angelinagnedina <46851279+angelinagnedina@users.noreply.github.com>
2022-03-25 03:27:30 +0300

    Update graph_initialization.py

--- bax/utils/graph_initialization.py
+++ bax/utils/graph_initialization.py
@@ -1,13 +1,6 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# # Какие опции будут доступны?
-
-# - Задать граф словарём, где ключ - id вершины, а значение - соседи вершины. Веса задаются функцией, переданной на вход
-# - Задать граф матрицей смежности, тогда id - номер вершины в столбце. Веса можно вписывать в ячейки матрицы или задать функцией
-# - Дать на вход готовый граф с весами
-# - Можно вызвать функцию make_grid_2d()
-
 
 
 import numpy as np
@@ -19,8 +12,18 @@ from matplotlib.collections import LineCollection as LC
 
 
 class Vertex:
-    def __init__(self, ind: int, position: np.array, neighbors = None):
-        # A vertex has a unique number, its coordinates and a list of its neighbors 
+    """
+        This class defines the vertex with all needed attributes.
+        
+        Attributes:
+            ind: The unique index of the vertex.
+            position: Coordinates of the vertex.
+            neighbors: Adjacent vertices.
+    """
+    
+    def __init__(self, ind: int, position, neighbors = None):
+        # A vertex has a unique number, its coordinates 
+        # and a list of its neighbors 
         self.ind = ind 
         self.position = position
         self.neighbors = [] if neighbors is None else neighbors
@@ -34,7 +37,16 @@ class Vertex:
 
 
 
-def make_ver(is_edge: np.array, pos: np.array):
+def make_ver(is_edge, pos):
+    """
+        Args:
+            is_edge: The adjacent matrix of the graph.
+            pos: Positions of the vertices.
+        
+        Returns:
+            Vertices that are instances of the class Vertex.
+    """
+    
     ver = [Vertex(i, p) for i, p in enumerate(pos)]
     for i in range(pos.shape[0]):
         for j in range(i + 1, pos.shape[0]):
@@ -47,6 +59,16 @@ def make_ver(is_edge: np.array, pos: np.array):
 
 
 def make_edge(ver):
+    """
+        Args:
+            ver: Vertices of the graph.
+        
+        Returns:
+            Edges (each edge is a tuple ((v_1, pos_1), (v_2, pos_2)), 
+            where v_1, v_2 are adjacent vertices and
+            pos_1, pos_2 are their positions).
+    """
+    
     edges = []
     for v in ver:
         for n in v.neighbors:
@@ -57,20 +79,44 @@ def make_edge(ver):
 
 
 def change_edges(edges):
+    """
+        Args:
+            edges: The output of the make_edge function.
+        
+        Returns:
+            Edges (each edge is a tuple ((v_1, v_2), pos), 
+            where v_1, v_2 are adjacent vertices and 
+            pos is the coordinates of the middle point between v_1 and v_2).
+    """
+    
     new_edges = []
     for e in edges:
         if e[0][0] < e[1][0]:
-            new_edges.append(((e[0][0], e[1][0]), np.sum([e[0][1], e[1][1]], axis = 0)/2))
+            new_edges.append(((e[0][0], e[1][0]), 
+                              np.sum([e[0][1], e[1][1]], axis = 0)/2))
     return new_edges
 
 
 
 
 def set_weights(edges, new_edges, func):
+    """
+        Args:
+            edges: The output of the make_edge function.
+            new_edges: The output of the change_edges function.
+            func: The cost function.
+        
+        Returns:
+            A dictionary that contains cost of each edge of the graph.
+            Keys are tuples (v_1_ind, v_2_ind), where v_1_ind, v_2_ind are
+            indices of the adjacent vertices and v_1_ind < v_2_ind.
+    """
+    
     weights = {}
     cnt = 0
     for i in range(len(edges)):
         if edges[i][0][0] < edges[i][1][0]:
+            # Make sure the function returns only non-negative weights            
             assert func(new_edges[cnt][1]) > 0
             weights[(edges[i][0][0], edges[i][1][0])] = func(new_edges[cnt][1])
             cnt += 1
@@ -86,19 +132,38 @@ def plot_graph(ax, edges, pos):
     ax.add_collection(lc)
 
     # plot vertices
-    ax.scatter(*pos.T, color=(0, 0, 0, 1), marker='.', facecolors='none', s=20)
+    ax.scatter(*pos.T, color=(0, 0, 0, 1), 
+               marker='.', facecolors='none', s=20)
 
     ax.grid(False)
     return
 
 
 
-def Rosenbrock(x, a = 1, b = 100):
+def rosenbrock(x, a = 1, b = 100):
+    """
+        An example of cost function from the paper.
+    """
+    
     return 0.01*((a - x[0])**2 + b*(x[1] - x[0]**2)**2) 
 
 
-class make_grid_2d:
-    def __init__(self, num_grid = (20, 20), x_lim = [-2, 2], y_lim = [-1, 4], func = Rosenbrock):
+class MakeGrid2d:
+    """
+    Class for initializing a 2d grid-shaped graph.
+    
+    Attributes:
+        num_grid: Number of vertices along x and y axes.
+        x_lim, y_lim: A limited area on the plane 
+          where the graph will be located. 
+        func: The cost function.
+        get_pos, get_ver, get_edges: Positions of the vertices, 
+          vertices and edges of the graph respectively.
+        weights: A dictionary that contains weights of the edges.
+    """
+    
+    def __init__(self, num_grid = (20, 20), x_lim = [-2, 2], 
+                 y_lim = [-1, 4], func = rosenbrock):
         self.num_grid = num_grid
         self.x_lim = np.array(x_lim)
         self.y_lim = np.array(y_lim)
@@ -107,43 +172,87 @@ class make_grid_2d:
         
     def __get__(self):
         num_grid_x, num_grid_y = self.num_grid
-        x, y = np.meshgrid(np.linspace(*self.x_lim, num_grid_x), np.linspace(*self.y_lim, num_grid_y))
-        self.positions = np.stack([x.flatten(), y.flatten()], axis = -1)
-        is_there_edge = [[False for _ in range(len(self.positions))] for _ in range(len(self.positions))]
+        x = np.meshgrid(np.linspace(*self.x_lim, num_grid_x)
+        y = np.linspace(*self.y_lim, num_grid_y))
+        positions = np.stack([x.flatten(), y.flatten()], axis = -1) 
+        pos_len = len(positions)
+        is_there_edge = [[False for _ in range(pos_len)] for _ in range(pos_len)]
         # Connecting vertices in directions N, E, S, W, NW, NE, SW, SE
-        for i in range(len(self.positions)):
-            for j in range(i + 1, len(self.positions)):
-                if (j - i == 1 and j % num_grid_x != 0) or (j == i + num_grid_x):
+        for i in range(pos_len):
+            for j in range(i + 1, pos_len):
+                if (j - i == 1 and j % num_grid_x != 0) or 
+                    (j == i + num_grid_x):
                     is_there_edge[i][j] = True
-                elif (j == i + num_grid_x + 1 and i % num_grid_x != num_grid_x - 1) or (j == i + num_grid_x - 1 and i % num_grid_x != 0):
+                elif (j == i + num_grid_x + 1 and 
+                      i % num_grid_x != num_grid_x - 1) or 
+                    (j == i + num_grid_x - 1 and 
+                     i % num_grid_x != 0):
                     is_there_edge[i][j] = True
         is_there_edge = np.array(is_there_edge)
-        self.vertices = make_ver(is_there_edge, self.positions)
-        self.edges = make_edge(self.vertices)
+        vertices = make_ver(is_there_edge, positions)
+        # This attribute is made for plot_ method        
+        self.edges = make_edge(vertices)
         new_edges = change_edges(self.edges)
         weights = set_weights(self.edges, new_edges, self.func)
 
-        return self.positions, self.vertices, new_edges, weights
+        return positions, vertices, new_edges, weights
     
     def plot_(self):
+        """
+            Method for visualizing the graph.
+        """
         edges = [(e[0][1], e[1][1]) for e in self.edges]
         fig, ax = plt.subplots(figsize=(8.9, 7))
         plot_graph(ax, edges, self.positions)
         ax.set(ylim=self.y_lim, xlim=self.x_lim) 
         plt.show()
     
-    def help(self):
-        print(f'num_grid is the size of your grid,\nx_lim and y_lim are limits within which the graph will exists,\nfunc should return weights of the edges')
 
 
 
 
-class graph_processing:
-    def __init__(self, graph = None, make_grid = False):
+class GraphProcessing:
+    """
+    Class for initializing an arbitrary graph.
+    
+    Attributes:
+        make_grid: True for initializing a 2d grid-shaped graph.
+        graph: If make_grid = True, then graph is an instance of MakeGrid2d,
+          else it is an instance of nx.classes.graph.Graph.
+        get_pos, get_ver, get_edges: Positions of the vertices, 
+          vertices and edges of the graph respectively.
+        weights: A dictionary that contains weights of the edges.
+    """
+    
+    def __init__(self, graph = None, make_grid = None):
+        """
+            Args:
+                graph: Either None (if make_grid = True), or one of the 
+                  dict/list/np.ndarray/nx.classes.graph.Graph types. 
+                  If dict, the keys should be the indices of the vertices 
+                  and the values should be the neighbors.
+                  If list/np.ndarray, the input is an adjacent matrix. 
+        """
+        
         self.graph = graph
         self.make_grid = make_grid
      
-    def create_graph(self, params = {}):
+    def create_graph(self, params = None):
+        """
+            Method that initializes a graph. 
+            
+            Args:
+                params: A dictionary of parameter values. If
+                  self.make_grid = True, it may contain attributes 
+                  for an instance of MakeGrid2d class. Else it may 
+                  contain the cost function as 'func' keyword (necessary 
+                  if self.graph is a list/np.ndarray object). For adjacent 
+                  matrix weights of the edges may be defined in cells of 
+                  the matrix. 
+        """
+        
+        if params is None:
+            params = {}
         G = self.graph
         make_grid = self.make_grid
         if make_grid:
@@ -163,10 +272,13 @@ class graph_processing:
             if params.get('func') is not None:
                 parameters.append(params['func'])
             else:
-                parameters.append(Rosenbrock)
-            graph = make_grid_2d(*parameters)
+                parameters.append(rosenbrock)
+            graph = MakeGrid2d(*parameters)
             self.graph = graph
-            self.get_pos, self.get_ver, self.get_edges, self.weights = graph.get_pos, graph.get_ver, graph.get_edges, graph.weights
+            self.get_pos = graph.get_pos
+            self.get_ver = graph.get_ver
+            self.get_edges = graph.get_edges
+            self.weights = graph.weights
         else:
             try:
                 assert G is not None
@@ -177,7 +289,7 @@ class graph_processing:
             for key in G.keys():
                 graph.add_node(key)
             for key, neigh in G.items():
-                if len(neigh) != 0:
+                if neigh:
                     for n in neigh:
                         graph.add_edge(key, n)               
         if type(G) == np.ndarray or type(G) == list:
@@ -213,17 +325,18 @@ class graph_processing:
                 for e in graph.edges:
                     weights[e] = graph.get_edge_data(*e)['weight']
             self.graph = graph
-            self.get_pos, self.get_ver, self.get_edges, self.weights = pos, ver, new_edges, weights 
+            self.get_pos = pos
+            self.get_ver = ver
+            self.get_edges = new_edges
+            self.weights = weights 
             
     def draw(self):
+        """
+            Method for visualizing the graph.
+        """
+        
         if self.make_grid:
             self.graph.plot_()
         else:
             nx.draw(self.graph)
             
-    def help(self):
-        print(f'If make_grid = True, then you may call create_graph\(\) method with dictionary of parameters, such as:\nsize,\nx_lim,\ny_lim,\nfunc ')
-        print('If make_grid = False, then there are multiple ways to create a graph: by dict/adjacency matrix/networkx graph')
-        print(f'If type\(graph\) == dict, then params[\'func\'] should be a function that returns the weights of the edges')
-        print('If you select adjacency matrix/networkx graph, then params[\'func\'] can be None and instead the weights should be in matrix/graph already')
-
